4c0993747a14967aaa6a860dc8b36e570672824a
Rails <railsG>
2015-07-30 17:31:08 +0530

    first commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:sessions][:email].downcase)
+    if user && user.authenticate(params[:sessions][:password])
+
+    if user.activated?
+      log_in user
+          params[:sessions][:remember_me] == '1' ? remember(user) : forget(user)
+      reddirct_back_or user
+        	else
+      message = "Account not activated"
+      message = "check your email for the activation link."
+      flash[:warning] = message
+      redirect_to root_url
+    end
+  else
+  		flash.now[:danger] = 'Invalid email/password combination'
+  		render 'new'
+  	end
+  end
+
+  def destroy
+    log_out if log_in(current_user)
+    redirect_to root_path
+  end
+end
